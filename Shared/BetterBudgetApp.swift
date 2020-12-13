6397df468bc8526a51d17325b6e94167e1350d05
Matt <mattmarks95@gmail.com>
2020-12-13 08:48:19 -0500

    Created new multiplatform SwiftUI project.

---
+++ Shared/BetterBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterBudgetApp.swift
+//  Shared
+//
+//  Created by Matt Marks on 12/13/20.
+//
+
+import SwiftUI
+
+@main
+struct BetterBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
